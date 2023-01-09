ccd24669db9fc11bb4b43b3f1724a28a9de07a23
Muskan123-lang <68841119+Muskan123-lang@users.noreply.github.com>
2023-01-09 20:04:31 +0530

    commit remaining files

---
+++ Program03.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace DataStructure03
+{
+    class Program03
+    {
+        public static int INT_MAX { get; private set; }
+
+        class Queue<T>
+        {
+            private T[] items;
+            private int front;
+            private int rear;
+            private int capacity;
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            public Queue()
+            {
+                this.capacity = 1000;
+                this.items = new T[this.capacity];
+                this.front = 0;
+                this.rear = 0;
+            }
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="capacity"></param>
+            public Queue(int capacity)
+            {
+                this.capacity = capacity;
+                this.items = new T[capacity];
+                this.front = 0;
+                this.rear = 0;
+            }
+            /// <summary>
+            /// Add item to the Queue
+            /// </summary>
+            /// <param name="item"></param>
+            public void Push(T item)
+            {
+
+
+                if (IsFull())
+                {
+                    throw new Exception("Queue is full");
+                }
+
+
+                // insert element at the rear 
+                items[rear] = item;
+                rear++;
+
+
+            }
+            /// <summary>
+            /// Returns the first item with deleting
+            /// </summary>
+            /// <returns></returns>
+            public T Pop()
+            {
+
+                // check if queue is empty 
+                if (IsEmpty())
+                {
+                    throw new Exception("Queue is empty");
+                }
+                T frontItem = items[this.front];
+                // shift elements to the right
+                for (int i = 0; i < rear - 1; i++)
+                {
+                    items[i] = items[i + 1];
+                }
+                rear--;
+                return frontItem;
+            }
+
+
+
+
+
+
+
+            /// <summary>
+            ///  Views the first element in the Queue but does not remove it.
+            /// </summary>
+            /// <returns></returns>
+            public T Peek()
+            {
+                return items[this.front];
+            }
+
+
+
+
+
+
+
+            /// <summary>
+            /// Contains value in Queue
+            /// </summary>
+            /// <param name="item"></param>
+            /// <returns></returns>
+            public bool Contains(T item)
+            {
+                for (int i = front; i < rear; i++)
+                {
+                    if (item.Equals(items[i]))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+
+
+
+
+            /// <summary>
+            /// Returns size
+            /// </summary>
+            /// <returns></returns>
+            public int Size()
+            {
+                return rear;
+            }
+
+
+
+
+
+            /// <summary>
+            /// Checks if Queue is empty
+            /// </summary>
+            /// <returns></returns>
+            public bool IsEmpty()
+            {
+                return front == rear;
+            }
+
+
+
+
+            /// <summary>
+            /// Checks if Queue is full
+            /// </summary>
+            /// <returns></returns>
+            public bool IsFull()
+            {
+                return capacity == rear;
+            }
+
+
+
+
+            /// <summary>
+            /// Reverse Queue
+            /// </summary>
+            public void Reverse()
+            {
+                T[] itemsTemp = new T[rear];
+                int counter = rear - 1;
+                for (int i = front; i < rear; i++)
+                {
+                    itemsTemp[counter] = items[i];
+                    counter--;
+                }
+
+
+                items = itemsTemp;
+            }
+
+
+            ///<summary>
+            ///     Middle of Queue
+            /// </summary>
+            public T findMiddle()
+            {
+                int i = 0;
+                while (i < (Size()/2))
+                {
+                    i++;
+                }
+                return items[i];
+            }
+
+
+
+
+
+            /// <summary>
+            /// Print Queue
+            /// </summary>
+            public void Print()
+            {
+                if (IsEmpty())
+                {
+                    throw new Exception("Queue is empty");
+                }
+
+
+                Console.WriteLine("Items in the Queue are:");
+                // traverse front to rear and print elements 
+                for (int i = front; i < rear; i++)
+                {
+                    Console.WriteLine(items[i]);
+                }
+            }
+
+            internal object ToList()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        class QueueIterator<T>
+        {
+            private Queue<T> currentQueue;
+
+
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="currentQueue"></param>
+            public QueueIterator(Queue<T> currentQueue)
+            {
+                this.currentQueue = currentQueue;
+            }
+
+
+
+
+            /// <summary>
+            ///     CHECK EMPTY OR NOT
+            /// </summary>
+            /// <returns></returns>
+            public bool IsEmpty()
+            {
+                return this.currentQueue.IsEmpty();
+            }
+
+
+
+            /// <summary>
+            /// DELETE ELEMENT
+            /// </summary>
+            /// <returns></returns>
+            public T Pop()
+            {
+                return this.currentQueue.Pop();
+            }
+        }
+        static void Main(string[] args)
+        {
+
+
+            try
+            {
+                Queue<int> myQueue = new Queue<int>(7);
+
+                //INSERT ELEMENTS
+                myQueue.Push(88);
+                myQueue.Push(78);
+                myQueue.Push(89);
+                myQueue.Push(23);
+                myQueue.Push(15);
+                myQueue.Push(24);
+                myQueue.Push(35);
+                myQueue.Print();
+                Console.WriteLine("Queue size is: {0}", myQueue.Size());
+                myQueue.Reverse();
+                Console.WriteLine("\nAfter Queue is Reversed");
+                myQueue.Print();
+
+
+                // MIDDLE ELEMENT
+                Console.WriteLine("\nMiddle element of Queue is : " + myQueue.findMiddle());
+
+
+
+                // CONTAINS ELEMENT OR NOT
+                if (myQueue.Contains(25))
+                {
+                    Console.WriteLine("Queue contains item 25");
+                }
+                else
+                {
+                    Console.WriteLine("Queue does not contain item 25");
+                }
+                if (myQueue.Contains(15))
+                {
+                    Console.WriteLine("Queue contains item 15");
+                }
+                else
+                {
+                    Console.WriteLine("Queue does not contain item 15");
+                }
+                
+
+
+                // DISPLAY PEEK ELEMENT
+                Console.WriteLine("The front item is {0}", myQueue.Peek());
+                
+                
+                // DELETE ELEMENTS
+                Console.WriteLine("Delete the front item: {0}", myQueue.Pop());
+                
+                
+                // SIZE
+                Console.WriteLine("Queue size is: {0}", myQueue.Size());
+
+
+
+
+                //  ITERATOR
+
+                QueueIterator<int> QueueIterator = new QueueIterator<int>(myQueue);
+                Console.WriteLine("\nDisplay items using QueueIterator");
+                while (!QueueIterator.IsEmpty())
+                {
+                    Console.WriteLine(QueueIterator.Pop());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program04.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace DataStructures04
+{
+    class Program04
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Creating priority queue");
+            var pq = new PriorityQueue<Student>();
+
+
+            var s1 = new Student("Aiden", 1.0);
+            var s2 = new Student("Baker", 2.0);
+            var s3 = new Student("Chung", 3.0);
+            var s4 = new Student("Dunne", 4.0);
+            var s5 = new Student("Eason", 5.0);
+            var s6 = new Student("Flynn", 6.0);
+
+
+            Console.WriteLine($"Adding {s5} to priority queue");
+            pq.Enqueue(s5);
+            Console.WriteLine($"Adding {s3} to priority queue");
+            pq.Enqueue(s3);
+            Console.WriteLine($"Adding {s6} to priority queue");
+            pq.Enqueue(s6);
+            Console.WriteLine($"Adding {s4} to priority queue");
+            pq.Enqueue(s4);
+            Console.WriteLine($"Adding {s1} to priority queue");
+            pq.Enqueue(s1);
+            Console.WriteLine($"Adding {s2} to priority queue");
+            pq.Enqueue(s2);
+
+
+            Console.WriteLine("\nPriority queue elements are:");
+            Console.WriteLine(pq.ToString());
+            Console.WriteLine($"\nPriority queue size is: {pq.Size()}");
+            Console.WriteLine();
+
+            pq.SortPriorityQueue();
+            Console.WriteLine("\nSorted Priority Queue: "+ pq.ToString());
+
+
+            Console.WriteLine($"\nPeeking element of priority queue: {pq.Peek()}");
+            Console.WriteLine($"Checking if priority queue contains peeked element: {pq.Contains(s1)}");
+
+
+            Console.WriteLine("\nRemoving element from priority queue");
+            var s = pq.Dequeue();
+            Console.WriteLine($"Removed element is {s}");
+            Console.WriteLine($"\nChecking if priority queue contains peeked element: {pq.Contains(s1)}");
+            Console.WriteLine("\nPriority queue is now:");
+            Console.WriteLine(pq.ToString());
+            Console.WriteLine();
+
+
+            Console.WriteLine("\nRemoving another element from priority queue");
+            s = pq.Dequeue();
+            Console.WriteLine($"Removed student is {s}");
+            Console.WriteLine("\nPriority queue is now:");
+            Console.WriteLine(pq.ToString());
+            Console.WriteLine();
+
+
+            Console.WriteLine("Printing the reversed priority queue:");
+            var reversed = pq.Reverse();
+            reversed.Print();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Iterating through the reversed priority queue:");
+            foreach (var elem in reversed)
+            {
+                Console.WriteLine(elem);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Middle element of Priority Queue : " + reversed.findMiddle());
+
+            Console.WriteLine("\nDequeueing elements from the reversed priority queue:");
+            Console.WriteLine(reversed.Dequeue());
+            Console.WriteLine(reversed.Dequeue());
+            Console.WriteLine(reversed.Dequeue());
+            Console.WriteLine(reversed.Dequeue());
+
+            Console.ReadLine();
+        }
+    }
+
+
+    public class Student : IComparable<Student>
+    {
+        public string LastName { get; }
+        public double Priority { get; set; } // smaller values are higher priority
+
+
+        public Student(string lastName, double priority)
+        {
+            LastName = lastName;
+            Priority = priority;
+        }
+
+
+        public override string ToString() => "(" + LastName + ", " + Priority.ToString("F1") + ")";
+
+
+        public int CompareTo(Student other)
+        {
+            if (Priority < other.Priority) return -1;
+            if (Priority > other.Priority) return 1;
+            return 0;
+        }
+    }
+
+
+    public class PriorityQueue<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private readonly List<T> data;
+        private readonly bool reversed;
+
+
+        public PriorityQueue(bool reversed = false)
+        {
+            data = new List<T>();
+            this.reversed = reversed;
+        }
+
+
+
+        /// <summary>
+        ///  insert elements in Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public void Enqueue(T item)
+        {
+            data.Add(item);
+            var ci = data.Count - 1; // child index; start at end
+            while (ci > 0)
+            {
+                var pi = (ci - 1) / 2; // parent index
+                var compared = data[ci].CompareTo(data[pi]);
+                if (!reversed && compared >= 0 || reversed && compared < 0)
+                    break; // child item is larger than (or equal) parent so we're done
+
+                Swap(ci, pi);
+                ci = pi;
+            }
+
+        }
+
+
+
+        /// <summary>
+        ///  delete element of Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public T Dequeue()
+        {
+
+            // assumes pq is not empty; up to calling code
+            var li = data.Count - 1; // last index (before removal)
+            var frontItem = data[0]; // fetch the front
+            data[0] = data[li];
+            data.RemoveAt(li);
+
+            li--; // last index (after removal)
+            var pi = 0; // parent index. start at front of pq
+            while (true)
+            {
+                var ci = pi * 2 + 1; // left child index of parent
+                if (ci > li) break; // no children so done
+
+
+                var rc = ci + 1; // right child                
+                if (rc <= li &&
+                    (!reversed && data[rc].CompareTo(data[ci]) < 0 || reversed && data[rc].CompareTo(data[ci]) >= 0)) // if there is a rc (ci + 1), and it is smaller than left child, use the rc instead
+                    ci = rc;
+
+                var comparedPi = data[pi].CompareTo(data[ci]);
+                if (!reversed && comparedPi <= 0 || reversed && comparedPi > 0)
+                    break; // parent is smaller than (or equal to) smallest child so done
+
+                Swap(pi, ci); // swap parent and child
+                pi = ci;
+            }
+            return frontItem;
+        }
+
+
+
+        /// <summary>
+        ///  display top most element of Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public T Peek() => data[0];
+
+
+
+
+
+        /// <summary>
+        ///     sort Priority Queue
+        /// </summary>
+        public void SortPriorityQueue() => data.Sort();
+
+
+
+        /// <summary>
+        ///  check if an element presents in Priority Queue or not
+        /// </summary>
+        /// <returns></returns>
+        public bool Contains(T elem)
+        {
+            return data.Contains(elem);
+        }
+
+
+
+        /// <summary>
+        ///  Reverse Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public PriorityQueue<T> Reverse()
+        {
+            var reversedQueue = new PriorityQueue<T>(true);
+            foreach (var elem in data)
+            {
+                reversedQueue.Enqueue(elem);
+            }
+            return reversedQueue;
+        }
+
+
+
+        /// <summary>
+        ///     middle element of a Priority Queue
+        /// </summary>
+        /// <returns></returns>
+
+        public T findMiddle()
+        {
+            int i = 0;
+            while (i < (Size()/2))
+            {
+                i++;
+            }
+            return data[i];
+        }
+
+
+
+        /// <summary>
+        ///  display size of Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public int Size() => data.Count;
+
+
+
+
+        /// <summary>
+        ///  display elements through Iterator of Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator() => data.GetEnumerator();
+
+
+        IEnumerator IEnumerable.GetEnumerator() => data.GetEnumerator();
+
+
+
+
+        /// <summary>
+        ///  display elements of Priority Queue
+        /// </summary>
+        /// <returns></returns>
+        public void Print() => Console.WriteLine(ToString());
+
+
+        public override string ToString()
+        {
+            var s = default(string);
+
+
+            foreach (var elem in data)
+                s += elem + " ";
+
+
+            return s;
+        }
+
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            var tmp = data[firstIndex];
+            data[firstIndex] = data[secondIndex];
+            data[secondIndex] = tmp;
+        }
+    }
+}
